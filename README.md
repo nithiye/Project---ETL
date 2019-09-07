81825507f836b7d7b2dabc20fb1f55066ca4106b
Nithya Iyengar <nithya.iyengar@gmail.com>
2019-09-07 18:51:23 -0500

    final updates

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Project-ETL
 Extract. Transform. Load
+
+###Extraction
+We used 3 datasets from the public platform of Inside Airbnb website. 
+
+The sources for our dataset are as follows:
+http://insideairbnb.com/get-the-data.html
+http://insideairbnb.com/seattle/
+
+Our dataset is as follows:
+1)	Listings.csv
+2)	Calendar.csv
+3)	Reviews.csv
+
+###Transformation
+Our first steps in cleaning up the datasets involved figuring out which variables were not relevant and filtering those out using Pandas (Jupyter Notebook).
+
+###Load
+The last step was to transfer our final output into a Database. We created an airbnb database and respective tables to match the columns from the final Panda’s Data Frame using MYSQL and then connected to the database using SQLAlchemy and loaded the result.
+
+
+
--- airbnb_db.sql
+++ airbnb_db.sql
@@ -1,3 +1,30 @@
+drop table if exists listings;
+drop table if exists reviews;
+drop table if exists calendar;
+
+
 SELECT * FROM listings;
 SELECT * FROM reviews;
 SELECT * FROM calendar;
+
+
+--List property type, amenities and review scores for each listing_id.
+SELECT listing_id, property_type, amenities, review_scores_rating
+FROM listings;
+
+-- List price per listing and neighborhood.
+select listings.listing_id, listings.neighborhood, AVG(calendar.price)
+from listings
+inner join calendar
+on listings.listing_id = calendar.listing_id
+group by listings.listing_id, listings.neighborhood
+order by listings.listing_id, listings.neighborhood ASC;
+
+-- list Comments by listing_id and review scores
+select listings.listing_id, listings.review_scores_rating, reviews.comments
+from listings
+inner join reviews
+on listings.listing_id = reviews.listing_id
+group by listings.listing_id, listings.review_scores_rating, reviews.comments
+order by listings.listing_id, listings.review_scores_rating ASC;
+
